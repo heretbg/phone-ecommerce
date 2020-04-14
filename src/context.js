65699414c7456a16de54f001b3c497ba781cecf1
Andrey Galeazzi <andrey_hp@hotmail.com>
2020-04-14 14:51:12 -0300

    - Added condition (inCart) to styled button; - Created handleDetail and getItem to display correct detail of clicked product;

--- src/context.js
+++ src/context.js
@@ -26,11 +26,20 @@ class ProductProvider extends Component {
         });
     };
 
-    handleDetail = () => {
-        console.log('hello from detail');
+    // get product by id
+    getItem = (id) => {
+        return this.state.products.find(item => item.id === id);
     };
-    addToCart = () => {
-        console.log('hello from add to cart');
+
+    handleDetail = (id) => {
+        const product = this.getItem(id);
+        this.setState(() => {
+            return {detailProduct: product}
+        })
+    };
+
+    addToCart = (id) => {
+        console.log('id is: ' + id);
     };
 
     render() {
