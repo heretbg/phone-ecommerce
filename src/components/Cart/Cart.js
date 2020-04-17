e3974882fa5e00e8606faabe73f60281fd20954b
Andrey Galeazzi <andrey_hp@hotmail.com>
2020-04-17 12:54:42 -0300

    - paypal sandbox integration started

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -17,10 +17,11 @@ class Cart extends Component {
                         if (cart.length > 0) {
                             return (
                                 <React.Fragment>
-                                    <Title name={"your"} title={"cart"} />
+                                    <Title name={"your"} title={"cart"}/>
                                     <CartColumns/>
                                     <CartList value={value}/>
-                                    <CartTotals value={value}/>
+                                    <CartTotals value={value}
+                                                history={this.props.history}/>
                                 </React.Fragment>
                             )
                         } else {
